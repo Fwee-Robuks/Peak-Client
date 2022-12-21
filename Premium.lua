26050b1faf8663b8a823a6239600700d9a556758
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2022-12-21 19:15:51 +1100

    Update Premium.lua

--- Premium.lua
+++ Premium.lua
@@ -303,7 +303,13 @@ local Button = brazil:CreateButton({
         loadstring(game:HttpGet("https://gist.githubusercontent.com/someunknowndude/18f1d979ad9a25ad69064be75f55f735/raw/dc36f1e9ad906a7434bd77bcd0ce8218fb5f4d88/zombie.lua"))()
 	end,
 })
-		
+
+local Button = brazil:CreateButton({
+	Name = "SCP 096 [HATS NEEDED]",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Solomonexy/SCP-096/main/Sussy%20SCP.lua"))()
+	end,
+})
 
 
 -- SCRIPT HUBS
